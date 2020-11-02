c726916a1292173e2c9bc3c3a4dee5164cd8ade4
hemants <fateh.singh@octalinfosolution.com>
2020-11-02 18:19:41 +0530

    design change

---
+++ app/src/main/java/com/os/busservice/ui/activity/CouponCodeActivity.kt
@@ -0,0 +1,54 @@
+package com.os.busservice.ui.activity
+
+import android.R.attr.label
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
+import androidx.core.app.ShareCompat
+import com.os.busservice.R
+import com.os.busservice.ui.baseFile.BaseBindingActivity
+import com.os.busservice.utility.AppDelegate
+import kotlinx.android.synthetic.main.activity_coupon_code.*
+import kotlinx.android.synthetic.main.activity_coupon_code.couponCode
+import kotlinx.android.synthetic.main.common_toolbar.*
+
+
+class CouponCodeActivity : BaseBindingActivity()  {
+    override fun setBinding() {
+     setContentView(R.layout.activity_coupon_code)
+    }
+
+    override fun createActivityObject() {
+        mActivity=this
+    }
+
+    override fun initializeObject() {
+        toolbarName.text = getString(R.string.coupon_code)
+        couponCode.text="FADFAD4"
+    }
+
+    override fun setListeners() {
+        toolbarBackBt.setOnClickListener { finish() }
+        copyTextLL.setOnClickListener {
+            val clipboard: ClipboardManager =
+                getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText(getString(R.string.copied_text), couponCode.text.toString())
+            clipboard.setPrimaryClip(clip)
+            AppDelegate.showToast(mActivity,getString(R.string.text_copy_successfully))
+        }
+
+        llShare.setOnClickListener {
+            val mShareText="Share Referral code for referral \n ${couponCode.text.toString()}"
+
+            ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setChooserTitle("Chooser title")
+                .setText(mShareText)
+                .startChooser();
+        }
+
+
+    }
+
+}
